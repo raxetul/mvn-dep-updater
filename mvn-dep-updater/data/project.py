b3a0bd9e7794ca1aca6210550f6db4f77f961bc9
Emrah URHAN <eurhan@infodif.com>
2018-09-11 17:02:57 +0300

    Clean up

--- mvn-dep-updater/data/project.py
+++
@@ -1,5 +0,0 @@
-class Project:
-    def __init__(self, projectName, artifactIdMapVersion, projectVersion):
-        self.projectName = projectName
-        self.artifactIdMapVersion = artifactIdMapVersion
-        self.projectVersion = projectVersion
